481ce35a350072aa4a3e80ae8af640ccce84bda5
Chad Bojelador <slsuls.chadbojelador@gmail.com>
2024-07-25 16:48:17 +0800

    declaration.cpp

---
+++ declaration.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+
+  int x= 5;
+  int y= 6;
+  double price = 10.55;
+  char grade = 'A';
+  bool student = true;
+
+  std::cout <<student << '\n';
+  std::cout << grade<< '\n';
+  std::cout << price<< '\n';
+  std::cout << x << '\n';
+  std::cout << y << '\n';
+ 
+ std::string name = "Chad";
+ std::cout<<name<<'\n';
+  return 0;
+}
